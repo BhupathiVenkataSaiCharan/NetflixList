8aeda7dd42849f0ad308f75fbac4c00ad5331cb1
Cherry <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-02-26 17:17:12 +0530

    Create Sdata.js

---
+++ src/Sdata.js
@@ -0,0 +1,35 @@
+const Sdata = [
+
+    {
+        imgsrc :"https://tse4.mm.bing.net/th?id=OIP.RPzD5Wy8rZVk5aPN1vbq_QHaEK&pid=Api&P=0&w=339&h=191",
+        title :"A Netflix Original Series",
+        sname : "Extra-curricular",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc :"https://tse2.mm.bing.net/th?id=OIP.Xd70RrrWT_IcZPq-gY77HgHaEK&pid=Api&P=0&w=329&h=186",
+        title :"A Netflix Original Series",
+        sname : "Erased",
+        link:"https://www.netflix.com/in/title/80173711"
+    },
+    {
+        imgsrc :"https://tse4.mm.bing.net/th?id=OIP.hjepgb0-3KA3aS5wYFXU8wHaEK&pid=Api&P=0&w=284&h=161",
+        title :"A Netflix Original Series",
+        sname : "Re:Mind",
+        link:"https://www.netflix.com/in/title/80198794"
+    },
+    {
+        imgsrc :"https://tse1.mm.bing.net/th?id=OIP.n4hgzJfhdPDM4yZguQO2igHaEe&pid=Api&P=0&w=256&h=155",
+        title :"A Netflix Original Series",
+        sname : "Alice in Borderland",
+        link:"https://www.netflix.com/in/title/80200575"
+    },
+    {
+        imgsrc :"https://tse4.mm.bing.net/th?id=OIP.kgTuKid1_8yhqoaG1_MW9QHaD4&pid=Api&P=0&w=362&h=191",
+        title :"A Netflix Original Series",
+        sname : "Baki",
+        link:"https://www.netflix.com/in/title/80204451"
+    },
+];
+
+export default Sdata;
